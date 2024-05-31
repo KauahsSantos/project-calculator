f62f4996e1842d1686399e0e5cfc6fa001adb35b
Kauã Santos <164094236+KauahsSantos@users.noreply.github.com>
2024-05-31 19:12:37 -0300

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,5 +4,11 @@ public class App {
         System.out.println("Testando ALterações");
         System.out.println("Será que ta rodando no GitHub???");
 
+        int a = 2;
+        int b = 2;
+        int c = a + b;
+
+        System.out.println(c);
+
     }
 }
