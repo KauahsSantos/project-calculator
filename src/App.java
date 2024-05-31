405a0919561230fab1dddbb180a4e3362054011e
Kauã Santos <kaua.hs.santos0100@gmail.com>
2024-05-31 19:09:13 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         System.out.println("Testando ALterações");
+        System.out.println("Será que ta rodando no GitHub???");
 
     }
 }
