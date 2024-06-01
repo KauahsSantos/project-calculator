ed2a17378eb09b0e828624b56fd15c7316398284
Kauã Santos <kaua.hs.santos0100@gmail.com>
2024-05-31 21:31:13 -0300

    Update Calculadora.java

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,14 +1,21 @@
+import java.util.*;
+
 public class Calculadora {
     public static void main(String[] args) throws Exception {
         
-        System.out.println("Testando ALterações");
-        System.out.println("Será que ta rodando no GitHub???");
+        Scanner sc1 = new Scanner(System.in);
+        
+        int n1;
+        int n2;
+        
+        System.out.println("Digite um número");
+        n1 = sc1.nextInt();
+        
+        System.out.println("Digite outro número");
+        n2 = sc1.nextInt();
+        
+        int n3 = n1 + n2;
 
-        int a = 2;
-        int b = 2;
-        int c = a + b;
-
-        System.out.println(c);
-
+        System.out.println(n3);
     }
 }
