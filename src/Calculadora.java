5fa1117a78887179aed536cc6775ea31da446f7c
Kauã Santos <kaua.hs.santos0100@gmail.com>
2024-05-31 22:40:17 -0300

    Update Calculadora.java

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -11,37 +11,41 @@ public class Calculadora {
         
         System.out.println("Digite um número");
         n1 = sc1.nextInt();
+
+        System.out.println("");
         
         System.out.println("Digite outro número");
         n2 = sc1.nextInt();
 
-        System.out.println("----------------");
+        System.out.println("");
 
+        System.out.println("----------------");
         System.out.println("1 | Soma");
         System.out.println("2 | Subtração");
         System.out.println("3 | Multiplicação");
         System.out.println("4 | Divisão");
+        System.out.println("----------------");
         n3 = sc1.nextInt();
+
+        System.out.println("");
 
         if(n3 == 1){
             n4 = n1 + n2;
 
-            System.out.println("A soma de:" + n1 + " + " + n2 + " = " + n4);
+            System.out.println("A soma de: " + n1 + " + " + n2 + " = " + n4);
 
         }if (n3 == 2) {
             n4 = n1 - n2;
 
-            System.out.println("A subtração de:" + n1 + " - " + n2 + " = " + n4);
+            System.out.println("A subtração de: " + n1 + " - " + n2 + " = " + n4);
         }if (n3 == 3){
             n4 = n1 * n2;
 
-            System.out.println("A multiplicação de:" + n1 + " * " + n2 + " = " + n4);
+            System.out.println("A multiplicação de: " + n1 + " * " + n2 + " = " + n4);
         }if (n3 == 4){
             n4 = n1 / n2;
 
-            System.out.println("A multiplicação de:" + n1 + " / " + n2 + " = " + n4);
-        }else{
-            System.out.println("Conta Não existe");
+            System.out.println("A multiplicação de: " + n1 + " / " + n2 + " = " + n4);
         }
 
     }
