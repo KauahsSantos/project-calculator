208d4d23a93c40ab27127aebef1644a8b845b45f
Kauã Santos <kaua.hs.santos0100@gmail.com>
2024-05-31 22:37:21 -0300

    Update Calculadora.java

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,6 +4,8 @@ public class Calculadora {
 
         int n1;
         int n2;
+        int n3;
+        int n4;
         
         Scanner sc1 = new Scanner(System.in);
         
@@ -12,11 +14,35 @@ public class Calculadora {
         
         System.out.println("Digite outro número");
         n2 = sc1.nextInt();
-        
-        int n3 = n1 + n2;
 
         System.out.println("----------------");
 
-        System.out.println(n3);
+        System.out.println("1 | Soma");
+        System.out.println("2 | Subtração");
+        System.out.println("3 | Multiplicação");
+        System.out.println("4 | Divisão");
+        n3 = sc1.nextInt();
+
+        if(n3 == 1){
+            n4 = n1 + n2;
+
+            System.out.println("A soma de:" + n1 + " + " + n2 + " = " + n4);
+
+        }if (n3 == 2) {
+            n4 = n1 - n2;
+
+            System.out.println("A subtração de:" + n1 + " - " + n2 + " = " + n4);
+        }if (n3 == 3){
+            n4 = n1 * n2;
+
+            System.out.println("A multiplicação de:" + n1 + " * " + n2 + " = " + n4);
+        }if (n3 == 4){
+            n4 = n1 / n2;
+
+            System.out.println("A multiplicação de:" + n1 + " / " + n2 + " = " + n4);
+        }else{
+            System.out.println("Conta Não existe");
+        }
+
     }
 }
